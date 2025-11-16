6637649c8c488a67758f6ed4876be87c2b2f2012
SULEIMAN SAID <real1sule@gmail.com>
2025-11-16 18:29:51 +0300

    Update magazine.py

--- lib/magazine.py
+++ lib/magazine.py
@@ -1,1 +1,36 @@
-g
+from .article import Article
+
+class Magazine:
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Name must be a string.")
+        if not 2 <= len(value) <= 16:
+            raise ValueError("Name must be between 2 and 16 characters.")
+        self._name = value
+
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Category must be a string.")
+        if len(value) == 0:
+            raise ValueError("Category must be longer than 0 characters.")
+        self._category = value
+
+    def articles(self):
+        return [article for article in Article.all if article.magazine == self]
+
+    def contributors(self):
+        return list({article.author for article in self.articles()})
